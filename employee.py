2971a23419a17697a152a0a25bb5a445d87168b7
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-10-16 23:13:48 +0800

    feat: adding employees dictionary list

---
+++ employee.py
@@ -0,0 +1,11 @@
+employees = [
+    {"name": "John Doe", "job_title": "Software Engineer"},
+    {"name": "Jane Smith", "job_title": "Data Analyst"},
+    {"name": "Peter Jones", "job_title": "Project Manager"},
+    {"name": "Mary Brown", "job_title": "Marketing Manager"},
+    {"name": "David Wilson", "job_title": "Sales Representative"}
+]
+
+print("\nEmployees:")
+for employee in employees:
+    print(f"Name: {employee['name']}, Job Title: {employee['job_title']}")
