a4447ba7d417b009c8a427b37e92f2d297f333e5
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-10-16 23:28:35 +0800

    feat: adding school university dictionary

---
+++ university.py
@@ -0,0 +1,11 @@
+universities = [
+    {"name": "Harvard University", "location": "Cambridge, Massachusetts"},
+    {"name": "Stanford University", "location": "Stanford, California"},
+    {"name": "MIT", "location": "Cambridge, Massachusetts"},
+    {"name": "Oxford University", "location": "Oxford, England"},
+    {"name": "Cambridge University", "location": "Cambridge, England"}
+]
+
+print("\nUniversities:")
+for university in universities:
+    print(f"Name: {university['name']}, Location: {university['location']}")
