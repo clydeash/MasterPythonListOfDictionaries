44a6b1eb9d7fb91d42441b060518c9d763550acf
Kisser Askley Endonela <kisserashleyendonela@gmail.com>
2024-10-16 23:22:39 +0800

    feat: adding book dictionary

---
+++ book.py
@@ -0,0 +1,12 @@
+books = [
+    {"title": "The Lord of the Rings", "author": "J.R.R. Tolkien"},
+    {"title": "Harry Potter and the Sorcerer's Stone", "author": "J.K. Rowling"},
+    {"title": "Pride and Prejudice", "author": "Jane Austen"},
+    {"title": "To Kill a Mockingbird", "author": "Harper Lee"},
+    {"title": "1984", "author": "George Orwell"}
+]
+
+
+print("\nBooks:")
+for book in books:
+    print(f"Title: {book['title']}, Author: {book['author']}")
